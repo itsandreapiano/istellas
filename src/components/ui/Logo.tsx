83695c152047f0d05429f0da9ad4feb284252d27
andreapiano <andreapiano1992@gmail.com>
2025-01-28 21:26:09 +0100

    fix: astro gold text classname in auth

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -2,7 +2,7 @@ const Logo = () => {
   return (
     <>
       <span className="text-astro-silver">i</span>
-      <span>Stellas</span>
+      <span className="text-astro-gold">Stellas</span>
     </>
   );
 };
