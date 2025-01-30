9df7e41d1f7554bf6ae2411a7d00608b84a9a6cb
andreapiano <andreapiano1992@gmail.com>
2025-01-30 13:11:59 +0100

    fix: test h-96 in chat when mobile

--- src/app/(main)/messages/Chat.tsx
+++ src/app/(main)/messages/Chat.tsx
@@ -25,7 +25,7 @@ const Chat = () => {
   }
 
   return (
-    <main className="relative h-auto w-full overflow-hidden rounded-2xl bg-card shadow-sm">
+    <main className="relative h-96 w-full overflow-hidden rounded-2xl bg-card shadow-sm mobile:h-auto">
       <div className="absolute bottom-0 top-0 flex w-full">
         <StreamChat
           client={chatClient}
